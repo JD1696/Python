55ccd3c8367ab3126ac3c711fe8d26bb1c19af1a
dillp <jdorr1696@gmail.com>
2022-02-06 18:37:42 -0500

    y

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from multiprocessing.connection import wait
 from operator import truediv
 import pygame, sys
 
@@ -11,68 +12,95 @@ WINDOW_SIZE = (1024, 768)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 player_image = pygame.image.load("groy.png")
 
-
+jumped = False
 moving_right = False
 moving_left = False
 xval = 0
+yval = 0
 player_location = [50, 50]
-player_y = 0
-player_x = 0
+gravity = True
 
-player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
-border_rect = pygame.Rect(0,0,1024,768)
+def moveFunction():
+        player_location[0] += xval
+        player_location[1] += yval
+
+    
+     
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 while True:
     screen.fill((146, 244, 255))
     screen.blit(player_image, player_location)
-    player_rect.x = player_location[0]
-    player_rect.y = player_location[1]
-    
+           
+           #Bouncy walls
 
-
-
-    if player_location[1] > WINDOW_SIZE[1] - player_image.get_height():
-        player_y = -player_y
-    else:
-        player_y += 0.2
-    player_location[1] += player_y
-
-
+    if player_location[1] > WINDOW_SIZE[1] - player_image.get_height() and gravity == True:
+       yval -= yval
+    if player_location[1] < 0:
+        print(yval)
+        yval = (yval)* -1
     if player_location[0] > WINDOW_SIZE[0] - player_image.get_width():
-        xval = (xval)*-1 
-        moving_right = False
-
+        xval = (xval) * -1
     if player_location[0] < 0:
-        xval = (xval)*-1 
-        moving_left = False
-
-    
-    def moveFunction():
+        xval = (xval) * -1
         
-        player_location[0] += xval
-
+       ############################################################
+    
     moveFunction()
-
-
+  
+    if xval > 0:
+        xval -= 0.05
     
-    
+    if xval < 0:
+        xval += 0.05
 
     if moving_right == True:
         xval += 0.2
-     
         
-       
     if moving_left == True:
         xval -= 0.2
-   
-        
 
-
+    if jumped == True:
+        yval -= 0.2
+        gravity = False
+    
+    if jumped == False:
+        gravity = True
+    
+      
+    if gravity == True:
+        yval += 0.2
 
     
         
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+ ####### inputs ###########
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -81,10 +109,14 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 moving_right = True
+            if event.key == K_UP:
+                jumped = True
                 
             if event.key == K_LEFT:
                 moving_left = True
         if event.type == KEYUP:
+            if event.key == K_UP:
+                jumped = False
             if event.key == K_RIGHT:
                 moving_right = False
             if event.key == K_LEFT:
