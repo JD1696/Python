4d542e476c3f9059999aeea33b5acdd03b06c9b6
dillp <jdorr1696@gmail.com>
2022-02-06 15:55:21 -0500

    asd

--- game.py
+++ game.py
@@ -1,48 +1,78 @@
 from operator import truediv
 import pygame, sys
+
 clock = pygame.time.Clock()
 
 from pygame.locals import *
-pygame.init() 
-pygame.display.set_caption('Game')
-WINDOW_SIZE = (1024,768)
-screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
-player_image = pygame.image.load('groy.png')
+
+pygame.init()
+pygame.display.set_caption("Game")
+WINDOW_SIZE = (1024, 768)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+player_image = pygame.image.load("groy.png")
 
 
 moving_right = False
 moving_left = False
-
-player_location = [50,50]
+xval = 0
+player_location = [50, 50]
 player_y = 0
+player_x = 0
+
+player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
+border_rect = pygame.Rect(0,0,1024,768)
+
 
 
 while True:
-    screen.fill((146,244,255))
-    screen.blit(player_image,player_location)
-   
-   
-    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
+    screen.fill((146, 244, 255))
+    screen.blit(player_image, player_location)
+    player_rect.x = player_location[0]
+    player_rect.y = player_location[1]
+    
+
+
+
+    if player_location[1] > WINDOW_SIZE[1] - player_image.get_height():
         player_y = -player_y
-    else: 
+    else:
         player_y += 0.2
     player_location[1] += player_y
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+
+
+    if player_location[0] > WINDOW_SIZE[0] - player_image.get_width():
+        xval = (xval)*-1 
+        moving_right = False
+
+    if player_location[0] < 0:
+        xval = (xval)*-1 
+        moving_left = False
+
+    
+    def moveFunction():
+        
+        player_location[0] += xval
+
+    moveFunction()
+
+
+    
+    
+
     if moving_right == True:
-        player_location[0] += 4
+        xval += 0.2
+     
+        
+       
     if moving_left == True:
-        player_location[0] -= 4
+        xval -= 0.2
+   
+        
 
 
+
+    
+        
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -51,6 +81,7 @@ while True:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 moving_right = True
+                
             if event.key == K_LEFT:
                 moving_left = True
         if event.type == KEYUP:
@@ -58,9 +89,6 @@ while True:
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
-            
-
-
 
     pygame.display.update()
     clock.tick(60)
